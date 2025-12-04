2e61438a27b4d4e155bddb90ff4ac03bab94087c
ilaXII <lg04.gabriel@gmail.com>
2025-12-04 14:30:56 +0100

    bug fix

--- src/main.c
+++ src/main.c
@@ -6,31 +6,38 @@
 int main(int argc, char *argv[]){
 	if (argc == 4)
 	{
-	char* op = argv[1];
-	char* a = argv[2];
-	char* b = argv[3];
-	double r = 0;
-	if (strcmp(op, "add") == 0) {
-		r = _add(atof(a), atof(b));
-		printf("%lf",r);
+		char* op = argv[1];
+		char* a = argv[2];
+		char* b = argv[3];
+		double r = 0;
+		if (strcmp(op, "add") == 0) {
+			r = _add(atof(a), atof(b));
+			printf("%lf",r);
+		}
+		else if (strcmp(op, "sub") == 0) {
+			r = _sub(atof(a), atof(b));
+			printf("%lf",r);
+		}
+		else if (strcmp(op, "mul") == 0) {
+			r = _mul(atof(a), atof(b));
+			printf("%lf",r);
+		}
+		else if (strcmp(op, "div") == 0) {
+			r = _div(atof(a), atof(b));
+			printf("%lf",r);
+		} 
+		else {printf("Erreur de parametres");}
 	}
-	else if (strcmp(op, "sub") == 0) {
-		r = _sub(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "mul") == 0) {
-		r = _mul(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "div") == 0) {
-		r = _div(atof(a), atof(b));
-		printf("%lf",r);
-	} 
-	else if (strcmp(op, "carre") == 0) {
-		r = _carre(atof(a));
-		printf("%lf",r);
-	}
-	else {printf("Erreur de parametres");}
+	else if (argc == 3)
+	{
+		char* op = argv[1];
+		char* a = argv[2];
+		double r = 0;
+		if (strcmp(op, "carre") == 0) {
+			r = _carre(atof(a));
+			printf("%lf",r);
+		} 
+		else {printf("Erreur de parametres");}
 	}
 	else {printf("Erreur de parametres");}
 	return 0;
